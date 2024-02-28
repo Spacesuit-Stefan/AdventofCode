abbe81dcbc29b67c419fa211065c4e27dc8973e3
Spacesuit-Stefan <yaspudboi@gmail.com>
2024-02-28 11:59:41 +0100

    finished day3 part 1

--- Day3/day3.py
+++ Day3/day3.py
@@ -10,42 +10,156 @@ inputFile = open('input.txt').read().strip()
 # add to sum
 
 lines = []
+partSum: int = 0
 
-# add each line to the list of lines
+
+    # add each line to the list of lines
 for line in inputFile.split('\n'):
     lines.append(line)
 
+def checkSymbol(charachter):
+    if (charachter == '.'):
+        return False
+    if (charachter.isdigit()):
+        return False
+    return True
 
-for i in range(len(lines)):
-    # print(lines[i])
-    # check first line for each character
-    chars = []
-    for char in lines[i]:
-        chars.append(char)
+def checkDigit(charachter):
+    if(charachter.isdigit()):
+        return True
+    return False
 
-    # check if line above/current/below has a character next to it or diagonally
-    for j in range(len(chars)):
-        if chars[j].isdigit():
-            # check left of digit (make sure it is not out of index)
-            if j-1 >= 0 and chars[j-1].isdigit():
-                print(chars[j-1])
+def checkIndex(row, index):
+    if(row < 0 < len(lines)):
+        return False
+    if (0 <= index < len(lines[row])):
+        return True
+    return False
 
-            # check right of digit (make sure it is not out of index)
-            if j+1 <= len(chars)-1 and chars[j+1].isdigit():
-                print(chars[j+1])
+def addTemp(row,index):
+    if (checkIndex(row, index)):
+        if (checkDigit(lines[row][index])):
+            return lines[row][index]
+    return None
+
+def checkLeft(row,index):
+    checkLeft = True
+    leftIndex = index
+    tempNumber = []
+    leftnumber = addTemp(row, leftIndex)
+    if (leftnumber != None):
+        tempNumber.insert(0, leftnumber)
+
+    while (checkLeft):
+        leftIndex = leftIndex - 1
+        leftnumber = addTemp(row, leftIndex)
+        if (leftnumber != None):
+            tempNumber.insert(0, leftnumber)
+        else:
+            return tempNumber
+
+def checkRight(row,index):
+    checkRight = True
+    rightIndex = index
+    tempNumber = []
+    while (checkRight):
+        rightIndex = rightIndex + 1
+        rightnumber = addTemp(row, rightIndex)
+        if ( rightnumber!= None):
+            tempNumber.append(rightnumber)
+        else:
+            return tempNumber
+
+
+def checkAbove(row,index):
+    global partSum
+    newRow = row-1
+    leftIndex = index
+    rightIndex = index -1
+    tempNumber = []
+    leftnumbers = checkLeft(newRow, index)
+    if (leftnumbers != None):
+        tempNumber.extend(leftnumbers)
+    #print(tempNumber, "aboveline2")
+    middlenumber = addTemp(newRow, index)
+    if (middlenumber == None):
+        if (len(tempNumber) > 0):
+            joinedList = int("".join(tempNumber))
+            #print(joinedList)
+            partSum += joinedList
+            tempNumber = []
+
+    rightnumbers = checkRight(newRow, index)
+    if (rightnumbers != None):
+        tempNumber.extend(rightnumbers)
+    #print(tempNumber, "aboveline2")
+    if (len(tempNumber) > 0):
+        joinedList = int("".join(tempNumber))
+        #print(joinedList)
+        partSum += joinedList
+
+
+def checkSameLine(row,index):
+    global partSum
+    leftIndex = index
+    rightIndex = index
+    tempNumber = []
+    leftnumbers = checkLeft(row, index)
+    if (leftnumbers != None):
+        tempNumber.extend(leftnumbers)
+    #print(tempNumber, "sameline1")
+    if (len(tempNumber) > 0):
+        joinedList = int("".join(tempNumber))
+        partSum += joinedList
+
+
+    tempNumber = []
+    rightnumbers = checkRight(row, index)
+    if (rightnumbers != None):
+        tempNumber.extend(rightnumbers)
+    #print(tempNumber, "sameline2")
+    if (len(tempNumber) > 0):
+        joinedList = int("".join(tempNumber))
+        partSum += joinedList
 
 
 
-            #check above line (make sure it exists)
-            if i-1 >= 0:
-                print()
-                # get the chars of that line
+def checkBelow(row,index):
+    global partSum
+    # go one below current row
+    newRow = row + 1
+    #[1,0,0]
+    tempNumber = []
+    leftnumbers = checkLeft(newRow, index)
+    if(leftnumbers != None):
+        tempNumber.extend(leftnumbers)
 
-            #check below line (make sure it exists)
-            if i+1 >= len(lines)-1:
-                print()
+    middlenumber = addTemp(newRow, index)
+    if (middlenumber == None):
+        if (len(tempNumber) > 0):
+            joinedList = int("".join(tempNumber))
+            partSum += joinedList
+            tempNumber = []
+
+    rightnumbers = checkRight(newRow, index)
+    if (rightnumbers != None):
+        tempNumber.extend(rightnumbers)
 
 
-# add numbers left and right to the number
-# add the number to the sum
+    #print(tempNumber, "below1")
+    if (len(tempNumber) > 0):
+        joinedList = int("".join(tempNumber))
+        #print (joinedList)
+        partSum += joinedList
+        #print(partSum)
 
+
+for row in range(0, len(lines)-1):
+    for index in range(0, len(lines[row])-1):
+        if(checkSymbol(lines[row][index])):
+            checkAbove(row, index)
+            checkSameLine(row, index)
+            checkBelow(row, index)
+
+
+print(partSum)
